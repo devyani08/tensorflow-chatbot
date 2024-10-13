179c6ef53aeba75a7f2b855cdf54bbaaebbcd357
Devyani Arya <devyaniarya5@gmail.com>
2024-10-14 04:01:31 +0530

    Create generate_files.py

---
+++ generate_files.py
@@ -0,0 +1,13 @@
+# to save the trained model
+model.save("chat_model.keras")
+# model.export("chat_model")
+
+import pickle
+
+# to save the fitted tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+# to save the fitted label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
